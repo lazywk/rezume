8a454ad806e3c3095b760d50723b26ee5ab541e2
lazywk <edoniyorbekdev@gmail.com>
2023-10-08 20:28:24 +0500

    portfolio responsive

--- src/components/elements/portfolio-card.js
+++ src/components/elements/portfolio-card.js
@@ -7,18 +7,20 @@ export default function PortfolioCard({ item }) {
                 <a target='blank' href={item.url}>
                     <img src={item.img} className='' alt={item.name} />
                 </a>
-                <p className='portfolio-card-task-title portfolio-card-lang-title'>Languages</p>
-                <div className='portfolio-card-task-list portfolio-card-lang-list'>
-                    {
-                        item.langs.map(el => <p className='portfolio-card-task-item portfolio-card-lang-item' key={el}>{el},</p>)
-                    }
+                <div>
+                    <p className='portfolio-card-task-title portfolio-card-lang-title'>Languages</p>
+                    <div className='portfolio-card-task-list portfolio-card-lang-list'>
+                        {
+                            item.langs.map(el => <p className='portfolio-card-task-item portfolio-card-lang-item' key={el}>{el},</p>)
+                        }
+                    </div>
                 </div>
             </div>
             <div className='portfolio-content'>
                 <div className='portfolio-card-header'>
                     <h3 className='portfolio-card-title'>
                         {item.name}</h3>
-                    <a target='blank' className='portfolioo-card-url' href={item.url}>Link</a>
+                    <a target='blank' className='portfolio-card-url' href={item.url}>Link</a>
                 </div>
                 <p className='portfolio-desc'>{item.description}</p>
                 <p className='portfolio-card-task-title'>Completed tasks</p>
