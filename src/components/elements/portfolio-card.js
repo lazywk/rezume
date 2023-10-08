c36a0480fb7f9e4d79a08a01813549148c87bbb9
lazywk <edoniyorbekdev@gmail.com>
2023-10-08 17:53:03 +0500

    complated portfolio page only dekstop

---
+++ src/components/elements/portfolio-card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function PortfolioCard({ item }) {
+    return (
+        <div className='portfolio-item portfolio-card'>
+            <div className='portfolio-card-img'>
+                <a target='blank' href={item.url}>
+                    <img src={item.img} className='' alt={item.name} />
+                </a>
+                <p className='portfolio-card-task-title portfolio-card-lang-title'>Languages</p>
+                <div className='portfolio-card-task-list portfolio-card-lang-list'>
+                    {
+                        item.langs.map(el => <p className='portfolio-card-task-item portfolio-card-lang-item' key={el}>{el},</p>)
+                    }
+                </div>
+            </div>
+            <div className='portfolio-content'>
+                <div className='portfolio-card-header'>
+                    <h3 className='portfolio-card-title'>
+                        {item.name}</h3>
+                    <a target='blank' className='portfolioo-card-url' href={item.url}>Link</a>
+                </div>
+                <p className='portfolio-desc'>{item.description}</p>
+                <p className='portfolio-card-task-title'>Completed tasks</p>
+                <div className='portfolio-card-task-list'>
+                    {
+                        item.tasks.map(el => <p className='portfolio-card-task-item' key={el}>{el}</p>)
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/user/portfolio.js
+++ src/pages/user/portfolio.js
@@ -1,9 +1,60 @@
+import PortfolioCard from 'components/elements/portfolio-card'
 import React from 'react'
 
 export default function PortfolioPage() {
+
+  const portfolioData = [
+    {
+      id: 1,
+      name: 'Boomerang - GPS Tracking System',
+      img: 'https://placehold.co/400x400',
+      url: 'https://gps.uzal.uz',
+      description: "A private website that controls the direction and movements of the cars, as well as the driver's duties, which are controlled by administrators.",
+      tasks: [
+        'Authentication',
+        'Working with 3 different roles (superadmin, admin, client)',
+        'Work with multi-location directions and routes using Google Map and Yandex Map',
+        'Strong Integrate with API',
+        'Socket.io',
+        'Responsive design'
+      ],
+      langs: [
+        'JavaScript',
+        'ReactJs',
+        'TailwindCSS'
+      ]
+    },
+    {
+      id: 2,
+      name: 'SOFF - ECommerce',
+      img: 'https://placehold.co/400x400',
+      url: 'https://soff.uz',
+      description: 'Providing a secure trading and secure payment system between the seller and the buyer',
+      tasks: [
+        'Partial participation in the project',
+        'Working with 3 different roles (admin, seller, customer)',
+        'Reviewing and fixing other people’s code',
+        'Explanation of errors and troubleshooting methods',
+        'Socket.io',
+        'Responsive design'
+      ],
+      langs: [
+        'NextJs',
+        'Ant Design',
+      ]
+    }
+  ]
+
   return (
-    <div>
-      Portfolio
+    <div className='portfolio'>
+      <h2 className='portfolio-title'>My Projects</h2>
+      <div className='portfolio-list'>
+        {
+          portfolioData.map(el => {
+            return <PortfolioCard item={el} key={el.id} />
+          })
+        }
+      </div>
     </div>
   )
 }
